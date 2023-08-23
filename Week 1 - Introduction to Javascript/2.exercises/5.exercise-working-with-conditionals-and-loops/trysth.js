6a2b5e6026e4976b4a0b5bcc820bc86bb96b038a
Klaus <whynotmemylove99@gmail.com>
2023-08-23 17:37:36 +0700

    walkthrough and finish week 1

---
+++ Week 1 - Introduction to Javascript/2.exercises/5.exercise-working-with-conditionals-and-loops/trysth.js
@@ -0,0 +1,7 @@
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles) //%c is a format specifier for CSS styling 
+}
+//https://developer.chrome.com/docs/devtools/console/format-style/
