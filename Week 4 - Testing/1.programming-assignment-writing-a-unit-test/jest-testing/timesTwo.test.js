6a5e1800195b02212f06ee243e8f753265da6559
Klaus <whynotmemylove99@gmail.com>
2023-08-25 07:25:50 +0700

    walkthrough week 4: testing with jest

--- Week 4 - Testing/1.programming-assignment-writing-a-unit-test/jest-testing/timesTwo.test.js
+++ Week 4 - Testing/1.programming-assignment-writing-a-unit-test/jest-testing/timesTwo.test.js
@@ -4,3 +4,7 @@ const timesTwo = require('./timesTwo');
 test('returns the number times 2', () => {
     expect(timesTwo(10)).toBe(20);
 });
+
+// cd to root folder jest-testing then: `npm install --save-dev jest`
+// `npm test` for testing all tests file in the project
+// `npm test timesTwo.test.js` for testing only this file
